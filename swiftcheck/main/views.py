a70927c805cd2f49936455545357585ae718130e
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2026-04-07 15:00:15 +0100

    categorize courses

--- swiftcheck/main/views.py
+++ swiftcheck/main/views.py
@@ -10,20 +10,15 @@ def login(request):
 
 def course(request):
     courses = Course.objects.all()
-    # frontend_courses = Course.objects.filter(category='FRONTEND')
-    # backend_courses = Course.objects.filter(category='BACKEND')
-    # fullstack_courses = Course.objects.filter(category='FULLSTACK')
-    print(courses)
-    
-    # print(frontend_courses)
-    # print(backend_courses)
-    # print(fullstack_courses)
+    frontend_courses = Course.objects.filter(category='FRONTEND')
+    backend_courses = Course.objects.filter(category='BACKEND')
+    fullstack_courses = Course.objects.filter(category='FULLSTACK')
 
     context = {
         'courses': courses,
-        # 'frontend_courses': frontend_courses,
-        # 'backend_courses': backend_courses,
-        # 'fullstack_courses': fullstack_courses,
+        'frontend_courses': frontend_courses,
+        'backend_courses': backend_courses,
+        'fullstack_courses': fullstack_courses,
     }
     return render(request, 'main/course.html', context)
 
